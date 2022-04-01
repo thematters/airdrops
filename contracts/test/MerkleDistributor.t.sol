27e2d459f04f4e86b43d45d3a84c293148085868
robertu <4065233+robertu7@users.noreply.github.com>
2022-04-01 17:25:40 +0800

    feat: add more tests

--- contracts/test/MerkleDistributor.t.sol
+++ contracts/test/MerkleDistributor.t.sol
@@ -19,9 +19,15 @@ contract MerkleDistributorTest is DSTest {
     uint256 constant EXPIRED_AT = 1648720000;
 
     bytes32 constant MERKLE_ROOT = 0xfc509a132749bf3defdee8950c44a9a83962d004f83c078c258dd3654fc540f2;
+    address ALICE = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address BOB = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
+    address CHARLIE = 0x7f268357A8c2552623316e2562D90e642bB538E5;
 
     function setUp() public virtual {
         vm.label(DEPLOYER, 'DEPLOYER');
+        vm.label(ALICE, 'ALICE');
+        vm.label(BOB, 'BOB');
+        vm.label(CHARLIE, 'CHARLIE');
 
         vm.startPrank(DEPLOYER);
 
@@ -52,37 +58,61 @@ contract MerkleDistributorTest is DSTest {
         assertEq(token.balanceOf(account), amount);
     }
 
+    /// @notice Alice and Bob claim successfully
     function testClaim() public {
         // Alice claims 100 $TOKEN
-        address ALICE = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
-        vm.label(ALICE, 'ALICE');
         bytes32[] memory aliceProof = new bytes32[](3);
         aliceProof[0] = 0x98bb949ca75e092f3e9c8e09d23063c64698f36937c6d2fe9c2144b8c4a1fbc2;
         aliceProof[1] = 0xe34ddb23ff40befd09095e53709edf85520e770273dab8b857fc60c68727003f;
         aliceProof[2] = 0x28f666abe594c6468cd5a251dd392713c3f01d0488c2598c3d0b72522aadf6ae;
+        vm.prank(ALICE);
         _claim(ALICE, 10e18, aliceProof);
 
         // Bob claims 0.32 $TOKEN
-        address BOB = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
-        vm.label(BOB, 'BOB');
         bytes32[] memory bobProof = new bytes32[](3);
         bobProof[0] = 0xd6ece33a93a757f3cb9ca7a599fa2f6912463168769e67e829ea5ba6b096e7b1;
         bobProof[1] = 0xe9530c925ead67ccd6c1c0b030ac7c15dfd76187946cdcf4ea3807650df62e91;
         bobProof[2] = 0x28f666abe594c6468cd5a251dd392713c3f01d0488c2598c3d0b72522aadf6ae;
+        vm.prank(BOB);
         _claim(BOB, 32e16, bobProof);
     }
 
+    /// @notice Alice and Bob claim successfully
+    function testFailClaimTwice() public {
+        // Alice claims 100 $TOKEN
+        bytes32[] memory aliceProof = new bytes32[](3);
+        aliceProof[0] = 0x98bb949ca75e092f3e9c8e09d23063c64698f36937c6d2fe9c2144b8c4a1fbc2;
+        aliceProof[1] = 0xe34ddb23ff40befd09095e53709edf85520e770273dab8b857fc60c68727003f;
+        aliceProof[2] = 0x28f666abe594c6468cd5a251dd392713c3f01d0488c2598c3d0b72522aadf6ae;
+
+        // success
+        vm.prank(ALICE);
+        _claim(ALICE, 10e18, aliceProof);
+
+        // failure
+        vm.prank(ALICE);
+        _claim(ALICE, 10e18, aliceProof);
+    }
+
+    /// @notice Charlie claims with wrong a proof
     function testFailClaim() public {
-        // Charlie claims 100 $TOKEN
-        address CHARLIE = 0x7f268357A8c2552623316e2562D90e642bB538E5;
-        vm.label(CHARLIE, 'CHARLIE');
+        bytes32[] memory wrongProof = new bytes32[](3);
+        wrongProof[0] = 0x98bb949ca75e092f3e9c8e09d23063c64698f36937c6d2fe9c2144b8c4a1fbc2;
+        wrongProof[1] = 0xe34ddb23ff40befd09095e53709edf85520e770273dab8b857fc60c68727003f;
+        wrongProof[2] = 0x28f666abe594c6468cd5a251dd392713c3f01d0488c2598c3d0b72522aadf6ae;
 
-        // wrong proof
-        bytes32[] memory charlieProof = new bytes32[](3);
-        charlieProof[0] = 0x98bb949ca75e092f3e9c8e09d23063c64698f36937c6d2fe9c2144b8c4a1fbc2;
-        charlieProof[1] = 0xe34ddb23ff40befd09095e53709edf85520e770273dab8b857fc60c68727003f;
-        charlieProof[2] = 0x28f666abe594c6468cd5a251dd392713c3f01d0488c2598c3d0b72522aadf6ae;
+        vm.prank(CHARLIE);
+        _claim(CHARLIE, 10e18, wrongProof);
+    }
 
-        _claim(CHARLIE, 10e18, charlieProof);
+    /// @notice Let Bob claim on behalf of Alice
+    function testBobClaimForAlice() public {
+        bytes32[] memory aliceProof = new bytes32[](3);
+        aliceProof[0] = 0x98bb949ca75e092f3e9c8e09d23063c64698f36937c6d2fe9c2144b8c4a1fbc2;
+        aliceProof[1] = 0xe34ddb23ff40befd09095e53709edf85520e770273dab8b857fc60c68727003f;
+        aliceProof[2] = 0x28f666abe594c6468cd5a251dd392713c3f01d0488c2598c3d0b72522aadf6ae;
+
+        vm.prank(BOB);
+        _claim(ALICE, 10e18, aliceProof);
     }
 }
